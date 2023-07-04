7fab04c8198e12e1d481d70c259dc90d8d5ff153
davidtanline <davidtanline@gmail.com>
2023-07-03 23:49:12 -0700

    Switch from mux to Fiber

--- server/main.go
+++ server/main.go
@@ -1,20 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+	"server/database"
+	"server/routes"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/gorilla/mux"
-
-	"server/controller"
+	"github.com/gofiber/fiber"
 )
 
 func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/getBatteries", controller.GetAllBatteries).Methods("GET")
-	http.Handle("/", router)
-	fmt.Println("Connected to port 3001")
-	log.Fatal(http.ListenAndServe(":3001", router))
+	database.Connect()
+
+	app := fiber.New()
+
+	routes.Setup(app)
+
+	app.Listen(":8000")
+	// router := mux.NewRouter()
+	// router.HandleFunc("/getBatteries", controller.GetAllBatteries).Methods("GET")
+	// http.Handle("/", router)
+	// fmt.Println("Connected to port 3001")
+	// log.Fatal(http.ListenAndServe(":3001", router))
 }
